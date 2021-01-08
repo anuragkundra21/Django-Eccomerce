58bb13338190a62dce86946955bfbd796ac98a60
anuragkundra21 <anurag.kundra21@gmail.com>
2021-01-09 03:04:11 +0530

    eight commit

--- mfw/urls.py
+++ mfw/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
